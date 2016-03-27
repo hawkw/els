0c4399d66861f0eccc620705494199a822ddedec
Hawk Weisman <hi@hawkweisman.me>
2016-03-27 13:32:20 -0400

    Added EMP arc to the Ra'ad

---
+++ data/scripts/weapons/RaadOnHit.java
@@ -0,0 +1,42 @@
+package data.scripts.weapons;
+
+import java.awt.Color;
+
+import org.lwjgl.util.vector.Vector2f;
+
+import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.CombatEntityAPI;
+import com.fs.starfarer.api.combat.DamageType;
+import com.fs.starfarer.api.combat.DamagingProjectileAPI;
+import com.fs.starfarer.api.combat.OnHitEffectPlugin;
+import com.fs.starfarer.api.combat.ShipAPI;
+
+public class RaadOnHit implements OnHitEffectPlugin {
+
+
+	public void onHit(DamagingProjectileAPI projectile,
+                      CombatEntityAPI target,
+					  Vector2f point,
+                      boolean shieldHit,
+                      CombatEngineAPI engine) {
+		if (shieldHit && target instanceof ShipAPI) {
+
+			float emp = projectile.getEmpAmount();
+			float dam = projectile.getDamageAmount() * 0.25f;
+
+			engine.spawnEmpArcPierceShields(projectile.getSource(),
+                                            point, target, target,
+            							    DamageType.ENERGY,
+            							    dam,
+            							    emp, // emp
+            							    100000f, // max range
+            							    "tachyon_lance_emp_impact",
+            							    20f, // thickness
+            							    new Color(248,255,85,255),
+            							    new Color(93,253,91,255)
+            							    );
+
+			//engine.spawnProjectile(null, null, "plasma", point, 0, new Vector2f(0, 0));
+		}
+	}
+}
